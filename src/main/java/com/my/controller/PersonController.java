479096f5c399aa11c6ab5fd741e77d7c89bfc669
张嘉辰 <1121401344@qq.com>
2020-06-19 20:33:00 +0800

    完成了用户信息的获取

---
+++ src/main/java/com/my/controller/PersonController.java
@@ -0,0 +1,95 @@
+package com.my.controller;
+
+import com.google.gson.Gson;
+import com.my.dao.PersonMapper;
+import com.my.dao.UserMapper;
+import com.my.pojo.Person;
+import com.my.pojo.PersonExample;
+import com.my.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@CrossOrigin
+@RequestMapping("/person")
+public class PersonController {
+
+    PersonMapper personMapper;
+    Gson gson = new Gson();
+
+    @Autowired
+    public void setPersonMapper(PersonMapper personMapper) {
+        this.personMapper = personMapper;
+    }
+
+    UserMapper userMapper;
+
+    @Autowired
+    public void setUserMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    @RequestMapping("/getInfo")
+    @ResponseBody
+    public String getInfo(@RequestParam("userId") String userId) {
+        Map<String, String> map = new HashMap<>();
+        if (userId != null) {
+            int id = Integer.parseInt(userId);
+            PersonExample personExample = new PersonExample();
+            personExample.createCriteria().andUserIdEqualTo(id);
+            List<Person> list = personMapper.selectByExample(personExample);
+            if (list.size() == 1) {
+                Person person = list.get(0);
+                Date date = person.getBirth();
+                map.put("code", "SUCCESS");
+                map.put("person", gson.toJson(person));
+                map.put("birth", new SimpleDateFormat("yyyy-MM-dd").format(date));
+                return gson.toJson(map);
+            }
+        }
+        map.put("code", "ERROR");
+        return gson.toJson(map);
+    }
+
+    @RequestMapping("/updateInfo")
+    @ResponseBody
+    public String updateInfo(@RequestParam("userId") String userId,
+                             @RequestParam("name") String name,
+                             @RequestParam("sex") String sex,
+                             @RequestParam("birth") String birth,
+                             @RequestParam("phone") String phone) {
+        Map<String, String> map = new HashMap<>();
+
+        int id = Integer.parseInt(userId);
+
+        Person person = new Person();
+        person.setName(name);
+        person.setSex(sex);
+
+        try {
+            person.setBirth(new SimpleDateFormat("yyyy-MM-dd").parse(birth));
+        } catch (ParseException e) {
+            return map.put("code", "ERROR");
+        }
+        PersonExample personExample = new PersonExample();
+        personExample.createCriteria().andUserIdEqualTo(id);
+        personMapper.updateByExampleSelective(person, personExample);
+        User user = new User();
+        user.setUserId(id);
+        user.setPhone(phone);
+        userMapper.updateByPrimaryKey(user);
+        return map.put("code", "SUCCESS");
+    }
+
+}
