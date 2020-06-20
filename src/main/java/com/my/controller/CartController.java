600b1031ef050429ec051f6e77404c5873d39e95
张嘉辰 <1121401344@qq.com>
2020-06-20 22:02:15 +0800

    购物车功能的编写

--- src/main/java/com/my/controller/CartController.java
+++ src/main/java/com/my/controller/CartController.java
@@ -2,14 +2,19 @@ package com.my.controller;
 
 import com.google.gson.Gson;
 import com.my.dao.CartMapper;
+import com.my.dao.GoodsMapper;
 import com.my.pojo.Cart;
+import com.my.pojo.CartExample;
+import com.my.pojo.Goods;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -22,6 +27,13 @@ public class CartController {
         this.cartMapper = cartMapper;
     }
 
+    GoodsMapper goodsMapper;
+
+    @Autowired
+    public void setGoodsMapper(GoodsMapper goodsMapper) {
+        this.goodsMapper = goodsMapper;
+    }
+
     Gson gson = new Gson();
 
     @ResponseBody
@@ -30,17 +42,37 @@ public class CartController {
                             @RequestParam("userId") int userId,
                             @RequestParam("num") int num) {
 
-        Cart cart = new Cart(null,userId,goodsId,num,0);
+        Cart cart = new Cart(null, userId, goodsId, num, 0);
         int line = cartMapper.insert(cart);
-        Map<String,String> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
 
-        if(line > 0){
-            map.put("code","SUCCESS");
-        }else{
-            map.put("code","ERROR");
+        if (line > 0) {
+            map.put("code", "SUCCESS");
+        } else {
+            map.put("code", "ERROR");
         }
         return gson.toJson(map);
+    }
 
-
+    @ResponseBody
+    @RequestMapping("/getCarts")
+    public String getCarts(@RequestParam("userId") int userId) {
+        CartExample cartExample = new CartExample();
+        cartExample.createCriteria().andUserIdEqualTo(userId);
+        List<Cart> cartList = cartMapper.selectByExample(cartExample);
+        Map<String,String> map = new HashMap<>();
+        if(cartList.size() > 0){
+            List<Goods> goodsList = new ArrayList<>();
+            for(Cart cart:cartList){
+                int goodId = cart.getGoodsId();
+                goodsList.add(goodsMapper.selectByPrimaryKey(goodId));
+            }
+            map.put("code","SUCCESS");
+            map.put("cartList",gson.toJson(cartList));
+            map.put("goodsList",gson.toJson(goodsList));
+        }else{
+            map.put("code","EMPTY");
+        }
+        return gson.toJson(map);
     }
 }
